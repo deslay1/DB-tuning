15b996c75a248a358eb9b234de03d35767a364d5
Osama Eldawebi <osama.eldawebi@gmail.com>
2022-02-04 09:12:34 +0000

    add 30Drw50 cassandra results

--- cass.py
+++
@@ -1,9 +0,0 @@
-from cassandra.cluster import Cluster
-import os
-
-cluster = Cluster()
-session = cluster.connect()
-
-session.execute("USE ycsb")
-# os.system("sleep 5s")
-session.execute("DROP TABLE ycsb.usertable")
